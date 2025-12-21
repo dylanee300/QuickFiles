d155e8fabf0e92fbc954a534bc5358a2cd0963c6
dylanee300 <dylanee300@gmail.com>
2025-12-21 08:47:03 -0800

    added checks to confirm deleting so I don't accidentally delete all the files in my desktop again

--- src/utils/Delete.cs
+++ src/utils/Delete.cs
@@ -1,32 +1,61 @@
 using System.IO;
+using System;
+using QuickFiles;
+using System.Windows.Forms;
 using System.Windows;
+using System.Linq.Expressions;
+// oh my spaghetti code help me pls
 
-
-namespace QuickFiles
+namespace QuickFiles.Utils
 {
-
     class DeleteFile
     {
         public DeleteFile(MainWindow mainWindow)
         {
-            string filePath = mainWindow.inputFilePathBox.Text.Trim();
-            if (filePath != "")
+            string filePath = Path.GetFullPath(mainWindow.inputFilePathBox.Text.Trim());
+
+            var confirm = MessageBox.Show(
+                "Are you sure you want to delete " + filePath + "?",
+                "Confirm",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning
+            );
+
+            if (confirm == MessageBoxResult.Yes)
             {
-                if (File.Exists(filePath))
+                try
                 {
-                    File.Delete(filePath);
-                    mainWindow.testOutput.Text = "file deleted: " + filePath;
-                } /* else
-                // commented out so I don't delete shit in my desktop again!! rip cs2, discord and other apps
-                {
-                    if (Directory.Exists(filePath))
+                    if (File.Exists(filePath))
                     {
-                        Directory.Delete(filePath, true);
-                        mainWindow.testOutput.Text = "dir deleted: " + filePath;
+                        File.Delete(filePath);
+                        MessageBox.Show("File deleted", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
+                    else
+                    {
+                        if (Directory.Exists(filePath))
+                        {
+                            Directory.Delete(filePath, true);
+                            MessageBox.Show("Directory deleted", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                        } else
+                        {
+                            MessageBox.Show(
+                                "File or dir not found",
+                                "Error",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error
+                            );
+                        }
+                    } 
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "Error deleting file: " + ex.Message,
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error
+                    );
                 }
-                */
-            } 
+            }
         }
     }
 }
